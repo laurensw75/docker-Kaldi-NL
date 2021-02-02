c228a6d49acc78b1753968b1b56f8900d247b4ac
laurensw75 <laurensw75@gmail.com>
2021-02-02 11:13:18 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -30,6 +30,7 @@ if [ "$YAML" == "" ] ; then
   exit 1;
 fi;
 
+export LD_PRELOAD=/opt/intel/mkl/lib/intel64/libmkl_def.so:/opt/intel/mkl/lib/intel64/libmkl_avx2.so:/opt/intel/mkl/lib/intel64/libmkl_core.so:/opt/intel/mkl/lib/intel64/libmkl_intel_lp64.so:/opt/intel/mkl/lib/intel64/libmkl_intel_thread.so:/opt/intel/lib/intel64_lin/libiomp5.so
 
 if [ "$MASTER" == "localhost" ] ; then
   # start a local master
